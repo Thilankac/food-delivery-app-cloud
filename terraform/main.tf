ca64a7db27d5cc2bea8c573067c066d625df7d82
Thilanka Chameera <Thilanka.Chameera@softlogiclife.lk>
2023-01-08 10:09:28 +0530

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,14 +33,6 @@
 #   sensitive = true
 # }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.96.0"
-    }
-  }
-}
 
 provider "azurerm" {
   features {}
