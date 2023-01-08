f058c6ed6fce38a91079918756ba1f7622a40c6b
Thilanka Chameera <Thilanka.Chameera@softlogiclife.lk>
2023-01-08 10:01:41 +0530

    add resource group

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstates"
-    storage_account_name = "kstfstateaccount003"
+    storage_account_name = "kstfstateaccount001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
